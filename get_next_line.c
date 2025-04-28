77c69bd4e671a85e2991e10bac21da86752e4ae2
Marcos <marcos29diciembre@gmail.com>
2025-04-28 19:50:52 +0200

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,20 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:49:17 by matoledo          #+#    #+#             */
-/*   Updated: 2025/04/28 16:58:01 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:07:45 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+//static variable needed for the "checkpoint" of where we are reading
 char	*get_next_line(int fd)
 {
-	
+	size_t			size = 20;
+	char			buffer[size];
+	static ssize_t	read_bytes;
+
+	read_bytes = read(fd, buffer, size);
+	if(read_bytes == -1)
+		return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:49:39 by matoledo          #+#    #+#             */
-/*   Updated: 2025/04/28 16:51:21 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:01:47 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
