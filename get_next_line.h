9cedcd00c261c86b8ac0672a20e3dc313707a115
Marcos <marcos29diciembre@gmail.com>
2025-05-10 20:24:42 +0200

    macro

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 16:49:39 by matoledo          #+#    #+#             */
-/*   Updated: 2025/05/02 11:12:35 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/05/06 14:19:44 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 # include <fcntl.h>
 # include <stdlib.h>
 //if BUFFER_SIZE is not define on the compiler with -D
+//-DDO_SOMETHING=1 - Equivalent to add to the code #define DO_SOMETHING = 1
 //create the macro with 42 value
+//macros normally cannot be changed, thats the main difference with global variables
+//when the code is compiled the macro is changed with the value choosed
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 512
 # endif
